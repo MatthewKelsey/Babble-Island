845d3e18a900a6cffbab0628d5b6d77ed1ee789d
Matthew Kelsey <matthewkelsey1985@gmail.com>
2023-02-03 15:36:34 +0000

    App moved to Game

--- client/src/App.js
+++ client/src/App.js
@@ -8,39 +8,19 @@ import Preloader from './components/scenes/preloader'
 import Map from './components/scenes/Map'
 import MiniGame1 from './components/scenes/MiniGame1'
 import MiniGame2 from './components/scenes/MiniGame2'
-
+import  Game  from "./Game";
 function App() {
 
-useEffect(()=>{
-  const config = {
-    type: Phaser.AUTO,
-    physics: {
-      default: 'arcade',
-      arcade: {
-        debug:true,
-        gravity: { y: 0},
-      },
-  
-    },
-    scene: [Preloader ,MiniGame1, MiniGame2,  Map],
-    scale: {
-      zoom: 1.5,
-      parent: 'phaser-game',
-      width: window.innerWidth,
-      height: window.innerHeight,
-    },
-  }
-  const game = new Phaser.Game(config)
-console.log(game)
-},[])
- 
+
   return (
 
+<BrowserRouter>
+<Routes>
+<Route path="/" element = {<Game />} /> 
 
-<div id="phaser-game">
-  I'm a happy React App!!!
+</Routes>
+</BrowserRouter>
 
-    </div>
   );
 }
 
---
+++ client/src/Game.js
@@ -0,0 +1,47 @@
+
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+// import Register from "./components/Register";
+import {useEffect} from 'react'
+import Phaser from 'phaser'
+import Preloader from './components/scenes/preloader'
+import Map from './components/scenes/Map'
+import MiniGame1 from './components/scenes/MiniGame1'
+import MiniGame2 from './components/scenes/MiniGame2'
+
+function Game() {
+
+useEffect(()=>{
+  const config = {
+    type: Phaser.AUTO,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug:true,
+        gravity: { y: 0},
+      },
+  
+    },
+    scene: [Preloader ,MiniGame1, MiniGame2,  Map],
+    scale: {
+      zoom: 1.5,
+      parent: 'phaser-game',
+      width: window.innerWidth,
+      height: window.innerHeight,
+    },
+  }
+  const game = new Phaser.Game(config)
+console.log(game)
+},[])
+ 
+  return (
+
+
+<div id="phaser-game">
+  I'm a happy React App!!!
+
+    </div>
+  );
+}
+
+export default Game;
