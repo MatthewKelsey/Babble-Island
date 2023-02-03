c0698bfa51d4ca8129e64ccd823e474a93c9b7dd
Marcella <marjory@hotmail.it>
2023-02-03 10:41:08 +0000

    map clean

--- client/src/components/src/Map.js
+++ client/src/components/src/Map.js
@@ -7,11 +7,11 @@ class Map extends Phaser.Scene {
       key: 'Map',
     });
   }
-  
+
   create() {
 
-    
-    
+
+
     // game.world.setBounds(0,0,2000,2000)
     const map = this.make.tilemap({ key: 'map' });
 
@@ -76,25 +76,6 @@ class Map extends Phaser.Scene {
     const layer0 = map.createLayer('sea', tileset);
     const layer = map.createLayer('water', tileset8);
 
-    const layer2 = map.createLayer('land', tileset2);
-    const layer1 = map.createLayer('trees', tileset3);
-    const layer4 = map.createLayer('objects', tileset4);
-    const layer5 = map.createLayer('houses', tileset5);
-    const layer6 = map.createLayer('doors', tileset6);
-    const layer7 = map.createLayer('roof', tileset5);
-    const layer8 = map.createLayer('bridges', tileset7);
-
-
-    layer1.setCollisionByProperty({ collisions: true });
-    // layer2.setCollisionByProperty({collisions:true});
-    layer.setCollisionByProperty({ collisions: true });
-    
-    layer4.setCollisionByProperty({ collisions: true });
-    layer5.setCollisionByProperty({ collisions: true });
-    layer6.setCollisionByProperty({ collisions: true });
-    layer7.setCollisionByProperty({ collisions: true });
-    // layer8.setCollisionByProperty({collisions:true});
-
 
     const land = map.createLayer('land', tileset2);
     const trees = map.createLayer('trees', tileset3);
@@ -116,7 +97,6 @@ class Map extends Phaser.Scene {
     door2.setCollisionByProperty({ collisions: true });
     door3.setCollisionByProperty({ collisions: true });
     door4.setCollisionByProperty({ collisions: true });
-    // layer6.setCollisionByProperty({ collisions: true });
     roof.setCollisionByProperty({ collisions: true });
 
 
